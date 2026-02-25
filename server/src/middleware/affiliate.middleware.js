8df8a69080957531028f3b9e5495dee84fb6d652
amutehjnr <mustaeenms@gmail.com>
2026-02-25 20:00:37 +0100

    fix all the neccessary validation

--- server/src/middleware/affiliate.middleware.js
+++ server/src/middleware/affiliate.middleware.js
@@ -71,7 +71,29 @@ const getAffiliateFromCookie = (req) => {
   return null;
 };
 
+const validate = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    
+    if (error) {
+      const errors = error.details.map(detail => ({
+        field: detail.path[0],
+        message: detail.message
+      }));
+      
+      return res.status(400).json({
+        success: false,
+        error: 'Validation failed',
+        errors
+      });
+    }
+    
+    next();
+  };
+};
+
 module.exports = {
   trackAffiliate,
   getAffiliateFromCookie,
+  validate,
 };
